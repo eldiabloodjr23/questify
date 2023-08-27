d26d46897be3ebc1b7b644023635fb75e750b664
elchakal23 <moneyemoji.pro@gmail.com>
2023-08-27 17:18:44 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import SignInButton from "@/components/SignInButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Víte v Questify X  🔥!</CardTitle>
+          <CardDescription>
+          Questify je platforma pro vytváření kvízů s využitím umělé inteligence! Začněte tím, že se přihlásíte níže!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google" />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/components/dashboard/HotTopicsCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import WordCloud from "../WordCloud";
+import { prisma } from "@/lib/db";
+
+type Props = {};
+
+const HotTopicsCard = async (props: Props) => {
+  const topics = await prisma.topic_count.findMany({});
+  const formattedTopics = topics.map((topic) => {
+    return {
+      text: topic.topic,
+      value: topic.count,
+    };
+  });
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">Žhavé témata</CardTitle>
+        <CardDescription>
+        Klikněte na téma, abyste mohli začít kvíz na dané téma.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pl-2">
+        <WordCloud formattedTopics={formattedTopics} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default HotTopicsCard;
